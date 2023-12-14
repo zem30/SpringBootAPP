45b7bdb1246a7613493088526003b94a312049e4
Alexe1Kal1n1n <kalinin.aleks199813@yandex.ru>
2023-12-14 15:54:59 +0400

    Initial commit

---
+++ src/test/java/ru/zem/springbootapp/springbootapp/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zem.springbootapp.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
